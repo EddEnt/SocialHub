3add852cfaf15b409b104834a8c0e2f7bd346f2c
EddEnt <61289536+EddEnt@users.noreply.github.com>
2024-02-13 16:49:07 -0500

    Commit of Activity, ActivityAttendee, AppUser and DataContext code rework. Still need to add the new Migrations table.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -7,9 +7,29 @@ namespace Persistence
     public class DataContext : IdentityDbContext<AppUser>
     {
         public DbSet<Domain.Activity> Activities { get; set; }
+        public DbSet<Domain.ActivityAttendee> ActivityAttendees { get; set; }
         public DataContext(DbContextOptions options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>(x => x.HasKey(
+                attendee => new { attendee.AppUserId, attendee.ActivityId }));
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(attendee => attendee.AppUser)
+                .WithMany(user => user.Activities)
+                .HasForeignKey(attendee => attendee.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(attendee => attendee.Activity)
+                .WithMany(activity => activity.Attendees)
+                .HasForeignKey(attendee => attendee.ActivityId);
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //For now, the database is stored in the project folder
